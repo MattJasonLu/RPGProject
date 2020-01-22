b27e629e0474a20466e5395b49024da74621652f
MurderousCrotch <1132464757@qq.com>
2020-01-22 22:15:33 +0900

    角色选择界面创建

---
+++ Assets/Scripts/start/BtnContainer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnContainer : MonoBehaviour
+{
+    // 1.游戏数据的保存，和场景之间游戏数据的传递使用PlayerPrefabs
+    // 2.场景的分类
+        // 2.1 开始场景
+        // 2.2 角色选择界面
+        // 2.3 游戏玩家打怪的界面，实际运行场景（村庄，野兽等）
+
+    // 开始新游戏
+    public void OnNewGame()
+    {
+        PlayerPrefs.SetInt("DataFromSave", 0);
+        // 加载我们的选择角色的场景
+
+    }
+
+    // 加载已经保存的游戏
+    public void OnLoadGame()
+    {
+        // 表示数据来自保存
+        PlayerPrefs.SetInt("DataFromSave", 1);
+        // 加载游戏玩家打怪界面
+    }
+}
---
+++ Assets/Scripts/start/PressAnyKey.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressAnyKey : MonoBehaviour
+{
+    // 表示是否有按键按下
+    private bool isAnyKeyDown = false;
+    private GameObject buttonContainer;
+
+    void Start()
+    {
+        buttonContainer = transform.parent.Find("btnContainer").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isAnyKeyDown)
+        {
+            if (Input.anyKey)
+            {
+                ShowButton();
+            }
+        }
+    }
+
+    void ShowButton()
+    {
+        buttonContainer.SetActive(true);
+        gameObject.SetActive(false);
+        isAnyKeyDown = true;
+    }
+}
