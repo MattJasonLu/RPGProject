0f666941ecb7e4786b93cd6378b58735fecc2c71
MurderousCrotch <1132464757@qq.com>
2020-01-22 20:47:31 +0900

    游戏雏形

---
+++ Assets/Scripts/MovieCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovieCamera : MonoBehaviour
+{
+    public float speed = 10;
+
+    private float endZ = -25;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 还没有达到目标位置，需要移动
+        if (transform.position.z < endZ)
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+    }
+}
