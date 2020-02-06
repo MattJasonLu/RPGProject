829273dec76b61e77f13899d8192a4c99bc719d1
MurderousCrotch <1132464757@qq.com>
2020-02-06 21:02:22 +0900

    武器商人

---
+++ Assets/Minimap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    private Camera minimapCamera;
+
+    private void Awake()
+    {
+        minimapCamera = GameObject.FindGameObjectWithTag(Tags.minimap).GetComponent<Camera>();
+    }
+
+    public void OnZoomInClick()
+    {
+        minimapCamera.orthographicSize--;
+    }
+
+    public void OnZoomOutClick()
+    {
+        minimapCamera.orthographicSize++;
+    }
+}
--- Assets/Scripts/custom/Tags.cs
+++ Assets/Scripts/custom/Tags.cs
@@ -9,4 +9,5 @@ public class Tags : MonoBehaviour
     public const string inventory_item = "InventoryItem";
     public const string inventory_item_grid = "InventoryItemGrid";
     public const string shortcut = "ShortCut";
+    public const string minimap = "Minimap";
 }
